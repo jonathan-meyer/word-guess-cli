c94bf60cbd4b089239fb6755a1df9805858bf714
Jonathan Meyer <jonathan.meyer@libertymutual.com>
2019-06-08 12:05:44 -0400

    chaged value to be a property added isSolved property added reveal method

--- src/Word.js
+++ src/Word.js
@@ -9,11 +9,24 @@ function Word(word) {
     throw "`word` argument required";
   }
 
-  this.value = word.split("").map(c => new Letter(c));
+  Object.defineProperty(this, "value", {
+    value: word.split("").map(c => new Letter(c)),
+    writable: false
+  });
+
+  Object.defineProperty(this, "isSolved", {
+    get: () => this.value.filter(l => !l.isVisible).length === 0
+  });
 }
 
 Word.prototype.toString = function() {
   return this.value.join(" ");
+};
+
+Word.prototype.reveal = function(letter) {
+  this.value
+    .filter(l => l.value.toLowerCase() === letter.toLowerCase())
+    .map(l => (l.isVisible = true));
 };
 
 module.exports = Word;
--- src/__tests__/Word.test.js
+++ src/__tests__/Word.test.js
@@ -12,9 +12,34 @@ describe("Word", () => {
     }).toThrow();
   });
 
-  it("", ()=>{
+  it("should not show a new word", () => {
+    const word = new Word("Fred Flintstone");
+    expect(word.toString()).toEqual("_ _ _ _   _ _ _ _ _ _ _ _ _ _");
+  });
+
+  it("should only show letters that are revealed", () => {
     const word = new Word("Fred Flintstone");
 
-    expect(word.toString()).toEqual("_ _ _ _   _ _ _ _ _ _ _ _ _ _")
-  })
+    word.reveal("f");
+    expect(word.toString()).toEqual("F _ _ _   F _ _ _ _ _ _ _ _ _");
+
+    word.reveal("l");
+    expect(word.toString()).toEqual("F _ _ _   F l _ _ _ _ _ _ _ _");
+
+    word.reveal("z");
+    expect(word.toString()).toEqual("F _ _ _   F l _ _ _ _ _ _ _ _");
+  });
+
+  it("should be solved if all letters are revealed", () => {
+    const word = new Word("cat");
+
+    word.reveal("c");
+    expect(word.isSolved).toBeFalsy();
+
+    word.reveal("a");
+    expect(word.isSolved).toBeFalsy();
+
+    word.reveal("t");
+    expect(word.isSolved).toBeTruthy();
+  });
 });
